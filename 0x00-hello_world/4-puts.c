ccf71f86aac5132bc13e9164e01497743fc048d6
MUTINDAH <petahmutinda@gmail.com>
2022-06-30 13:04:59 -0700

    edit 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(void) {
 
-char str[] = "\"Programming is like building a multilingual puzzle";
 
-puts(str);
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-return 0;
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
