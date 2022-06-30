7800c85067fcfa324d3494f6355d5d61953b73de
MUTINDAH <petahmutinda@gmail.com>
2022-06-30 13:28:35 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-/* entry point */
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
+/**
+ *
+ *  * main func entry point
+ *
+ *   */
 
-  return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
 }
