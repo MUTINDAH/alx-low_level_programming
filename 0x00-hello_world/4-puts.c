1a04168b388b3a172cb4b89ae8404101ddb7631e
MUTINDAH <petahmutinda@gmail.com>
2022-06-30 13:18:56 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
 
-
+#!/bin/bash
 #include <stdio.h>
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+/* entry point */
 
-int main(void)
+int main(void){
+  puts("\"Programming is like building a multilingual puzzle");
 
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+  return (0);
 }
