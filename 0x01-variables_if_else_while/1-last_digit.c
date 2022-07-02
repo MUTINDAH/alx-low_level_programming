fc6b23abae341760f0bceec9c41bd90051b3d367
MUTINDAH <petahmutinda@gmail.com>
2022-07-02 05:13:05 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 l = n % 10;
 if (n > 5)
 {
-printf("Last digit of %d is %i and is greater than 5\n", n, l);
+printf("Last digit of %i is %i and is greater than 5\n", n, l);
 }
 else if (n == 0)
 {
-printf("Last digit of %d is %i and is 0\n", n, l);
+printf("Last digit of %i is %i and is 0\n", n, l);
 }
 else if (n < 6)
 {
-printf("Last digit of %d is %i and is less than 6 and not 0\n", n, l);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
