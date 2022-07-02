f9442b3f1b3c423f87c98431a422326bf1460045
MUTINDAH <petahmutinda@gmail.com>
2022-07-02 04:32:31 -0700

    push edit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,9 +19,9 @@ else if (n == 0)
 {
 printf("Last digit of %i is 0 and is 0\n", n);
 }
-else if (n < 6)
+else if (n < 6 !== 0)
 {
-printf("Last digit of %i is %i less than 6 and not 0 and is less than 6 and not 0\n", n, l);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
