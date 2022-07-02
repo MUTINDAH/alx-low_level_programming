fe247477b4c59e6f7983cb133cb445c1204734f5
MUTINDAH <petahmutinda@gmail.com>
2022-07-02 06:14:32 -0700

    add new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: This program will assign a random number
+ * to the variable n each time it is executed
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for(ch = 'a'; ch = "z"; ch++)
+{
+putchar("%c", ch);  
+}
+return(0);
+}
