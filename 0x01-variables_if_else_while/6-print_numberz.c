a94902caf74ea9772f461052379d8844370ae2a6
MUTINDAH <petahmutinda@gmail.com>
2022-07-04 01:32:30 -0700

    new file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for(x == 0; x < 10; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
