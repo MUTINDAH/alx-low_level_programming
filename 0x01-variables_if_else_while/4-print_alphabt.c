1500b45a6ead2ecc3dc1d84c7e73b9bf86c96ab9
MUTINDAH <petahmutinda@gmail.com>
2022-07-04 00:47:37 -0700

    new file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-entry point of program
+ * return-success return 0
+ */
+int main (void)
+{
+	char ch;
+	for (ch = 0;ch <= 0; ch++)
+	{
+		if ( ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	return (0);
+}
