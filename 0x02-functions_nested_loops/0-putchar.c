574f9a1a59152e1f27459f0fbdaa08a9dd8d8fc7
MUTINDAH <petahmutinda@gmail.com>
2022-07-05 14:04:33 -0700

    edit file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
 *
 * Return: nothing
 */
-int main(void)
+void printputchar(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	return ();
 }
