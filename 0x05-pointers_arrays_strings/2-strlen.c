c6687870cedabf6aa8867359dc4817d442de4d9e
MUTINDAH <petahmutinda@gmail.com>
2022-07-12 13:30:30 -0700

    2 file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @s : character to find length.
+ * Return:void
+ */
+
+int _strlen(char *s);
+{
+	strlen(*s);
+}
