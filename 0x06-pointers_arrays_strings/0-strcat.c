40ac6eb80149edbb1c55126ae7b9834dec96833d
MUTINDAH <petahmutinda@gmail.com>
2022-07-14 10:31:22 -0700

    0 file edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
- * *_strcat - function that swaps the values of two integers
- * @dest : integers to be swapped.
- * @src : integers to be swapped.
- * Return:void
- */
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
 
 char *_strcat(char *dest, char *src)
 {
-char *dest;
-char *src;
-for (dest = s1; *dest != 0; dest++) 
-{
-/* Skip along to find the end */
-}
-for (src = s2; *src != 0; dest++, src++ )
-{
-*dest = *src;
-}
-*dest = 0; /* Set the last character to 0 */
-return(dest);
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
+return (dest);
 }
