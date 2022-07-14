681102af004e5867211920fda5ecf3c8fcaa8879
MUTINDAH <petahmutinda@gmail.com>
2022-07-14 01:36:32 -0700

    first file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strcat - prints reversed string, followed by a new line
+ * @dest: pointer to the string to "+"
+ * @src: pointer to the string to "+"
+ * Return: void
+*/
+char *_strcat(char *dest, char *src)
+{
+	strcat(*dest, *src);
+}
