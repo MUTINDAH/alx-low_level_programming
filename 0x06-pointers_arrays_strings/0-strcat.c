8a4e58e8fec5dca6a6c399b26dcb1fd8dfe1b0a7
MUTINDAH <petahmutinda@gmail.com>
2022-07-14 06:01:16 -0700

    0 file edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,4 @@
-#include "main.h"
-/**
- * *_strcat - prints reversed string, followed by a new line
- * @dest: pointer to the string to "+"
- * @src: pointer to the string to "+"
- * Return: void
-*/
-char *_strcat(char *dest, char *src)
-{
-	strcat(*dest, *src);
-}
+VimCrypt~03!Œå‡ˆå*‘Ğë^SØa%·i¶@{ÿ¢|]Jv’íÿ-/vPBÉÚ¨N*tÃ®CÊå³Í};Ø4¯+é›[]R*ÈàÔLCLj¯à±r}¥ ñøù`Ğñ®ƒ'èpÆréL|áJ¦ë‰²iÄ|Ó÷gL“è¡Uk‘ı‡.è_¶O’'¸ÿl³¡÷‹P£yl{±š£˜
+R"ó?nv(¶Â8›÷Õ E·8B['Ü®îÇô÷–ÇISoÛ:ïÁ¨uêpÿ ©ô0_O\< ¤˜ì=R[Û8Ä(’ò\2Ã^…’Û!_ª­6U
+Æ"Æ®R¬M~æC	¨ê]æIÇøÃI´[ü\)™	šTÚ"”R´Xó,iÃ“¬ìjš´‹
+b£Ôß§JŸÈáØ_òzØŠìY]£à‚”ÙN@o7Qi¸›YÊ3òù3Q6«_cxK#ûJ–o”ÃªKÑ{%‘I/zd¾€UA´8ïo*iïÂ+ı®ÕF‡{/ÁĞÖèšöÒ]Ûªv“ä%hÜw»O*Ñ›İTÔHğ(#úÆt
