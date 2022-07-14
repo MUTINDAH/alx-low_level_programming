d7eb2af26097f55aa7381f96fcf8b00c9af4c520
MUTINDAH <petahmutinda@gmail.com>
2022-07-14 16:04:47 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
-VimCrypt~03!Œå‡ˆå*‘Ðë^SØa%·i¶@{ÿ¢|]Jv’íÿ-/vPBÉÚ¨N*tÃ®ŽCÊå³Í};Ø4¯+é›[]R*ÈàÔLCLj¯à±r}¥ ñøù`Ðñ®ƒ'èpÆréL|áJ¦ë‰²iÄ|Ó÷gL“è¡Uk‘ý‡.è_¶O’'¸ÿl³¡÷‹P£yl{±š£˜
-R"ó?nv(¶Â8›÷Õ E·8B['Ü®îÇô÷–ÇISoÛ:ïÁ¨uêpÿ ©ô0_O\< ¤˜ì=R[Û8Ä(’ò\2Ã^…’Û!_ª­6U
-Æ"Æ®R¬M~æC	¨ê]æIÇøÃI´[ü\)™	šTÚ"”R´Xó,iÃ“¬ìjš´‹
-b£Ôß§JŸÈáØ_òzØŠìY]£à‚”ÙN@o7Qi¸ž›YÊ3òù3Q6«_cxK#ûJ–o”ÃªKÑ{%‘I/zd¾€UA´8ïo*iïÂ+ý®ÕF‡{/ÁÐÖèšöÒ]Ûªv“ä%hÜw»O*Ñ›ÝTÔHð(#úÆt
-=======
-#include "main.h"
-/**
- * *_strcat - prints reversed string, followed by a new line
- * @dest: pointer to the string to "+"
- * @src: pointer to the string to "+"
- * Return: void
-*/
 char *_strcat(char *dest, char *src)
 {
-	char *rdest = dest;
-	while (*dest)
-		dest++;
-	while (*dest++ = *src++)
-		;
-	return rdest;
+char *dest;
+char *src;
+for (dest = s1; *dest != 0; dest++) 
+{
+/* Skip along to find the end */
 }
->>>>>>> 1974919ece58c5a4123516add18ae892fd264b99
+for (src = s2; *src != 0; dest++, src++ )
+{
+*dest = *src;
+}
+*dest = 0; /* Set the last character to 0 */
+}
