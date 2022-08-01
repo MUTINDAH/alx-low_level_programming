ec9b4af0529047718d85ea65a4977917ada5b993
MUTINDAH <petahmutinda@gmail.com>
2022-08-02 01:19:03 +0300

    modified dog.h and added init_dog file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - initializes struct dog
+ * @d : param d
+ * @name : param name
+ * @age : param age
+ * @owner : param owner
+ * return : no
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
