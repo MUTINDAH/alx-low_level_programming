b6399db51981b6b765a6652b528239942ca67554
MUTINDAH <petahmutinda@gmail.com>
2022-08-03 16:12:39 +0300

    edit 2 files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * print_name - function that prints a name
  * @name : name param to print
@@ -10,7 +10,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
 	f(name);
 }
