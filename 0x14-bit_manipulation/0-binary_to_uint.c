79f65336f394fc39e73005466daad233610046d1
MUTINDAH <petahmutinda@gmail.com>
2022-08-18 23:05:09 +0300

    modified all files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * binary_to_int - converts a binary to int
+ * binary_to_uint - converts a binary to int
  * @s: pointer to binary chars
+ *
  * Return: int value or zero
  */
 unsigned int binary_to_uint(const char *s)
